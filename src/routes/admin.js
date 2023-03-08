c5cc460dedd2a631d85d7244722f9c8d1128a4b7
Goster210 <juan.rincon12@uptc.edu.co>
2023-03-08 13:12:29 -0500

    configuracion inicial -> inicio de sesion con json web token

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,6 +1,7 @@
 const {Router} = require('express');
 const router = Router();
 const {signIn, signOut } = require('../controller/admin');
+const { authenticateToken } = require("../helper/jwt");
 
 
 //inicio de sesion
