566d166d7e2098ed294f5cf70c26a0995732c2d3
Goster210 <juan.rincon12@uptc.edu.co>
2023-03-17 18:05:49 -0500

    codificacion para la funcionalidad del crud de servicios

--- src/controller/service.js
+++ src/controller/service.js
@@ -29,11 +29,72 @@ const crearServicio = async (req, res) => {
   }
 };
 
-const listarServicios = async (req, res) => {};
+const listarServicios = async (req, res) => {
+  try {
+    const servicios = await service.find();
+    return res.status(200).json({
+      succes: true,
+      servicios,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      succes: false,
+      error: error.message,
+    });
+  }
+};
 
-const eliminarServicio = async (req, res) => {};
+const eliminarServicio = async (req, res) => {
+  
+  const { nameService } = req.body;
 
-const actualizarServicio = async (req, res) => {};
+  try {
+    const deleteItem = await service.findOneAndDelete({ nameService });
+
+    return res.status(200).json({
+      succes: true,
+      deleteItem,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      succes: false,
+      error: error.message,
+    });
+  }
+};
+
+const actualizarServicio = async (req, res) => {
+
+  const { nameService, subService, linkImg } = req.body;
+
+  try {
+
+    let find = await service.findOne({ nameService });
+
+    if (find) {
+      const servicioUpdate = await service.findOneAndUpdate(
+        { nameService },
+        req.body,
+        {
+          new: true,
+        }
+      );
+      return res.status(200).json({
+        succes: true,
+        servicioUpdate,
+      });
+    }
+    return res.status(400).json({
+      succes: false,
+      error: "Ese servicio no existe",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      succes: false,
+      error: error.message,
+    });
+  }
+};
 
 module.exports = {
   crearServicio,
