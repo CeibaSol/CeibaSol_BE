b147b6765c9f7cbe1799fb3982ab2469c248c7a7
DAVIDTS0286 <67129995+DAVIDTS0286@users.noreply.github.com>
2023-03-29 08:45:15 -0500

    implemetacion de JWT para los endpoints de trabajos realizados

--- src/routes/works.js
+++ src/routes/works.js
@@ -1,17 +1,18 @@
 const { Router } = require('express');
 const router = Router();
+const { authenticateToken } = require("../helper/jwt");
 
 const { crearTrabajo, listarTrabajos, verTrabajo, eliminarTrabajo, actualizarTrabajo } = require('../controller/works')
 
 //crear trabajo relizado
-router.post('/crearTrabajo', crearTrabajo);
+router.post('/crearTrabajo', [authenticateToken], crearTrabajo);
 //listar trabajo relizado
 router.get('/listarTrabajos', listarTrabajos);
 //ver un trabajo realizado
 router.get('/verTrabajo', verTrabajo);
 //eliminar trabajo relizado
-router.delete('/eliminarTrabajo', eliminarTrabajo);
+router.delete('/eliminarTrabajo', [authenticateToken], eliminarTrabajo);
 //actualizar trabajo relizado
-router.put('/actualizarTrabajo', actualizarTrabajo);
+router.put('/actualizarTrabajo', [authenticateToken], actualizarTrabajo);
 
 module.exports = router;
